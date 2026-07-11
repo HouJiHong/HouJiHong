ba888aa181d94ac6abc3c7b537259513922fcafe
Hjh <2256718717@qq.com>
2026-07-11 16:05:34 +0800

    zuoye

---
+++ src/com/HJh/test/SolutionXuanZhuan.java
@@ -0,0 +1,37 @@
+package com.HJh.test;
+
+class SolutionXuanZhuan {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SolutionXuanZhuan xz = new SolutionXuanZhuan();
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        xz.rotate(matrix);
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
